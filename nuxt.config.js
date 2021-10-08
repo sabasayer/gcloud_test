96dff297948b7bfe7165990c3d247a116c29c612
salih <salihbakisayer@hotmail.com.tr>
2021-10-08 13:33:44 +0300

    feat(.env): use .env API_KEY

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'test',
+    title: `test - ${process.env.API_KEY}`,
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
